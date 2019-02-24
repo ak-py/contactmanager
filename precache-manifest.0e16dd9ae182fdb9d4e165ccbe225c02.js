a2031cf79e9e33b8b372d913c4004cfa27cc91b8
Akshit Gupta <akshitgupta1996@gmail.com>
2019-02-23 20:51:23 -0800

    Updates

---
+++ precache-manifest.0e16dd9ae182fdb9d4e165ccbe225c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "9bc9e4218b7a288136e4",
+    "url": "/contactmanager/static/js/main.9bc9e421.chunk.js"
+  },
+  {
+    "revision": "0bb3510500eaa75686b5",
+    "url": "/contactmanager/static/js/2.0bb35105.chunk.js"
+  },
+  {
+    "revision": "9bc9e4218b7a288136e4",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0bb3510500eaa75686b5",
+    "url": "/contactmanager/static/css/2.4c0bb9e5.chunk.css"
+  },
+  {
+    "revision": "ded38bac6cfa926f3b84ae1b510d4a6d",
+    "url": "/contactmanager/index.html"
+  }
+];
